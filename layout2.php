8b06203a3c410b3384150353f756e1afcdc0a1e0
excellencetechnologies <excellencetechnologies@gmail.com>
2017-01-23 11:49:30 +0530

    initial commit

---
+++ layout2.php
@@ -0,0 +1,109 @@
+<!doctype html>
+<html>
+ <head>
+  <title>Layout2 Media Query</title>
+  <meta http-equiv="Content-Type" content="text/html; charset=windows-1252">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link href="css/layout2.css" type="text/css" rel="stylesheet" />
+ </head>
+ <body>
+  <div class="menu_div">
+  <label for="menu-toggle" ><img id="img" src="image/list.png" height="30px" width="30px" > </label>
+  <input type="checkbox" id="menu-toggle" />
+   <ul id="menu">
+    <li><a href="#" class="link_1">Layout 3</a></li>
+    <li><a href="#" class="link_2">About</a></li>
+    <li><a href="#" class="link_3">Services</a></li>
+    <li><a href="#" class="link_3">Contact</a></li>
+  </ul>
+ </div>
+  <div id="sample-heading">
+   <br />
+   <h1>Sample Heading</h1>
+   <br />
+   <p class="para">Lorem ipsum dolor sit amet, consectetur adipiscing elit.Ipsa,ipsam, eligendi, in quo sunt possimus non incidunt odit vero aliquid similique quaerat nam nobis illo aspernatur vitae fugiat numquam repellat.</p>
+   <br /><br />
+   <input type="button" value="Call to action" id="call_to_action_button" />
+  </div>
+  <br />
+  <br />
+  <hr id="top_hr" />
+  <br />
+  <br />
+  <p id="latest_features">Latest Features</p>
+  <br />
+  <div id="blocks">
+   <div id="block1">
+    <img src="image/image2.png" class="image">
+    <div id="feature_label1">
+     <br />
+     <center><p class="feature_label">Feature Label</p></center>
+     <br />
+     <center><p class="para2">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p></center>
+    </div>
+    <div class="buttons">
+     <center>
+      <br />
+      <input type="button" value="Buy Now!" class="buy_now_button" />
+      <input type="button" value="More Info" class="more_info_button" />
+     </center>
+    </div>
+    <br />
+   </div>
+   <div id="block2">
+    <img src="image/image2.png" class="image">
+    <div id="feature_label2">
+     <br />
+     <center><p class="feature_label">Feature Label</p></center>
+     <br />
+     <center><p class="para2">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p><center>
+    </div>
+    <div class="buttons">
+     <br />
+      <center>
+       <input type="button" value="Buy Now!" class="buy_now_button" />
+       <input type="button" value="More Info" class="more_info_button" />
+      </center>
+    </div>
+    <br />
+   </div>
+   <div id="block3">
+    <img src="image/image2.png" class="image">
+    <div id="feature_label3">
+     <br />
+     <center><p class="feature_label">Feature Label</p></center>
+     <br />
+     <p class="para2">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+    </div>
+    <div class="buttons">
+     <br />
+     <center>
+      <input type="button" value="Buy Now!" class="buy_now_button" />
+      <input type="button" value="More Info" class="more_info_button" />
+     </center>
+    </div>
+    <br />
+   </div>
+   <div id="block4">
+    <img src="image/image2.png" class="image">
+    <div id="feature_label4">
+     <br />
+     <center><p class="feature_label">Feature Label</p></center>
+     <br />
+     <center><p class="para2">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p></center>
+    </div>
+    <div class="buttons">
+     <br />
+      <center>
+       <input type="button" value="Buy Now!" class="buy_now_button" />
+       <input type="button" value="More Info" class="more_info_button" />
+      </center>
+    </div>
+    <br />
+   </div>
+  </div>
+  <hr id="bottom_hr" />
+  <p id="copyright">Copyright &copy; Your Website 2014</p>
+  <br /><br /><br />
+ </body>
+</html>
